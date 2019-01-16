ea067f6406443e024dc7b1b3c8d96156bae736b1
deo enrico <deo.enrico@gmail.com>
2019-01-15 17:03:33 -0800

    cool

--- camera.py
+++ camera.py
@@ -3,11 +3,12 @@ from datetime import datetime
 from signal import pause
 
 camera = PiCamera()
-take = int(input("Type a number> "))
 
 def capture():
     datetime = datetime.now().isoformat()
     camera.capture('/home/pi/%s.jpg' % datetime)
+
+take = int(input("Type a number> "))
 
 if take = 1:
     camera.start_preview()

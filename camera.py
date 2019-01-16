1abf3c5b16f394c44cb68d1d4d812c3ed73d0cba
deo enrico <deo.enrico@gmail.com>
2019-01-15 16:42:02 -0800

    whatever

---
+++ camera.py
@@ -0,0 +1,18 @@
+from picamera import PiCamera
+from datetime import datetime
+from signal import pause
+
+camera = PiCamera()
+
+def capture():
+    datetime = datetime.now().isoformat()
+    camera.capture('/home/pi/%s.jpg' % datetime)
+
+take = int(input("Hit 1 to take picture!"))
+
+if take = 1:
+    capture()
+else:
+    pause()
+
+pause()

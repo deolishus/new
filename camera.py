0487b7f64c33abbf93e3d2a82a1b424317cbcdfe
deo enrico <deo.enrico@gmail.com>
2019-01-16 08:40:55 -0800

    On branch master

--- camera.py
+++ camera.py
@@ -1,22 +1,8 @@
 from picamera import PiCamera
-from datetime import datetime
-from signal import pause
+from time import sleep
 
 camera = PiCamera()
 
-def capture():
-    datetime = datetime.now().isoformat()
-    camera.capture('/home/pi/%s.jpg' % datetime)
-
-    take = int(input("Type a number> "))
-
-    if take == 1:
-        camera.start_preview
-
-    elif take == 2:
-        camera.stop_preview
-    else:
-        camera.capture
-
-
-pause()
+camera.start_preview()
+sleep(10)
+camera.stop_preview()

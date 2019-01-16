adaf312c227876737d8a00f17c92b1650e456090
deo enrico <deo.enrico@gmail.com>
2019-01-16 12:07:22 -0800

    On branch master

--- camera.py
+++ camera.py
@@ -1,14 +1,8 @@
 from picamera import PiCamera
 from time import sleep
 
-input = int(input("1 to quit."> ))
-
 camera = PiCamera()
 
 camera.start_preview()
-
-
-if input = 1:
-    camera.stop_preview()
-else:
-    print("smile!")
+sleep(10)
+camera.stop_preview()

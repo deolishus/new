f91f6e3aa3bd235b68d21ebee73c997a13cfd30f
deo enrico <deo.enrico@gmail.com>
2019-01-16 16:09:14 -0800

    On branch master

--- camera.py
+++ camera.py
@@ -4,5 +4,7 @@ from time import sleep
 camera = PiCamera()
 
 camera.start_preview()
-sleep(900000)
+camera.start_recording('/home/pi/video.h264')
+sleep(10)
+camera.stop_recording()
 camera.stop_preview()

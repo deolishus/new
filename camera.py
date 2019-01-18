028e62e7cce84be0465118b724b8e677d88a6913
deo enrico <deo.enrico@gmail.com>
2019-01-18 13:36:51 -0800

    camera

--- camera.py
+++ camera.py
@@ -1,11 +1,6 @@
 from picamera import PiCamera
 from time import sleep
 
-camera = PiCamera()
-camera.annotate_text = "Hello world!"
-camera.image_effect = 'cartoon'
 camera.start_preview()
-camera.start_recording('/home/pi/video.h264')
-sleep(5)
-camera.stop_recording()
+pause(2000000)
 camera.stop_preview()
